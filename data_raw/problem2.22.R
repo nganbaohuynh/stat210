be50c09263eca9d27d43ceff817fb85930e4abd4
nganbaohuynh <xiu.huynhngan@gmail.com>
2021-06-10 14:08:39 +0200

    updated factor

--- data_raw/problem2.22.R
+++ data_raw/problem2.22.R
@@ -1,2 +1,6 @@
 load("data_raw//example14.16.RData")
-usethis::use_data(example14.16, overwrite=FALSE)
+usethis::use_data(example14.16, overwrite=TRUE)
+
+summary(example14.16)
+example14.16$replicate <- as.factor(example14.16$replicate)
+
